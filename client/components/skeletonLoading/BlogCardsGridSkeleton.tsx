86bbb5dd7c4e1c56b5d2103fdbfaa120008e6ce7
Harish143-dev <saravananharish143@gmail.com>
2025-12-18 13:06:57 +0530

    login rerender fix

--- client/components/skeletonLoading/BlogCardsGridSkeleton.tsx
+++ client/components/skeletonLoading/BlogCardsGridSkeleton.tsx
@@ -1,11 +1,16 @@
+import { Skeleton } from "../ui/skeleton";
 import BlogCardSkeleton from "./BlogCardSkeleton ";
 
 export const BlogCardsGridSkeleton = ({ count = 6 }: { count?: number }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 px-4 md:px-14">
-      {Array.from({ length: count }).map((_, i) => (
-        <BlogCardSkeleton key={i} />
-      ))}
+    <div className="w-full px-4 md:px-14 py-12 overflow-hidden">
+      <Skeleton className="h-9 w-64" />
+      <Skeleton className="h-5 w-96 mt-2" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+        {Array.from({ length: count }).map((_, i) => (
+          <BlogCardSkeleton key={i} />
+        ))}
+      </div>
     </div>
   );
 };
