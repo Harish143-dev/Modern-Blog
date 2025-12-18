db7da80d82b83d7d88e96dd98f85f7e389746987
Harish143-dev <saravananharish143@gmail.com>
2025-12-18 12:59:16 +0530

    login rerender fix

--- client/components/skeletonLoading/BlogCardsGridSkeleton.tsx
+++ client/components/skeletonLoading/BlogCardsGridSkeleton.tsx
@@ -2,7 +2,7 @@ import BlogCardSkeleton from "./BlogCardSkeleton ";
 
 export const BlogCardsGridSkeleton = ({ count = 6 }: { count?: number }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 px-4 md:px-14">
       {Array.from({ length: count }).map((_, i) => (
         <BlogCardSkeleton key={i} />
       ))}
